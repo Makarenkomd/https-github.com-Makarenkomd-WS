b518c2e1cb3786de4e796ee891751396f461d9d0
MD <mahadrum@yandex.ru>
2019-03-04 14:26:10 +0300

    Выделили функцию отображения 3-х картинок

--- MainScreen/FormMainScreen.cs
+++ MainScreen/FormMainScreen.cs
@@ -15,26 +15,6 @@ namespace MainScreen
         public FormMainScreen()
         {
             InitializeComponent();
-            showPicture();
-        }
-        int numberCurrentPicture = 0;
-        void showPicture()
-        {
-            pictureBox1.Image = Image.FromFile("images/"+((numberCurrentPicture) %18 + 1)+".jpg");
-            pictureBox2.Image = Image.FromFile("images/"+ ((numberCurrentPicture+1) % 18 + 1) + ".jpg");
-            pictureBox3.Image = Image.FromFile("images/"+ ((numberCurrentPicture+2) % 18 + 1) + ".jpg");
-        }
-
-        private void buttonRight_Click(object sender, EventArgs e)
-        {
-            numberCurrentPicture += 3;
-            showPicture();
-        }
-
-        private void buttonLeft_Click(object sender, EventArgs e)
-        {
-            numberCurrentPicture = (numberCurrentPicture +18 - 3)%18;
-            showPicture();
         }
     }
 }
